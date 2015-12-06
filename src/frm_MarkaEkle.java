82de2c7bdd35d1df3988fc6376f2027e5e452e3f
kgulle <kenangulle@hotmail.com>
2015-12-06 14:13:13 +0200

    v5

--- src/frm_MarkaEkle.java
+++ src/frm_MarkaEkle.java
@@ -1,6 +1,8 @@
 
 import java.awt.*;
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.*;
 import org.postgresql.*;
 
@@ -10,23 +12,41 @@ import org.postgresql.*;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author kenan
  */
 public class frm_MarkaEkle extends javax.swing.JFrame {
-    Connection conn=null;
-    PreparedStatement pst=null;
-    Statement stmt = null;
 
+    Connection conn = null;
+    PreparedStatement pst = null;
+    Statement stmt = null;
+    ResultSet rs = null;
+
+    private void markalariGetir() {
+        DefaultListModel DLM = new DefaultListModel();
+        String sql = "select * from markalar order by marka_id";
+        try {
+            pst = conn.prepareStatement(sql);
+            rs = pst.executeQuery();
+            while (rs.next()) {
+                DLM.addElement(rs.getString("marka_ad"));
+            }
+            lst_markalar.setModel(DLM);
+            //  DLM=null;
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e);
+        }
+    }
+    
+    
 
     public frm_MarkaEkle() {
         initComponents();
-        conn=javaconnect.ConnecrDB();
-
+        conn = javaconnect.ConnecrDB();
+        markalariGetir();
     }
-
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -36,13 +56,22 @@ public class frm_MarkaEkle extends javax.swing.JFrame {
         cmd_markaEkle = new javax.swing.JButton();
         txt_marka = new javax.swing.JTextField();
         jLabel1 = new javax.swing.JLabel();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jList1 = new javax.swing.JList<>();
         jPanel2 = new javax.swing.JPanel();
         txt_markaDuzenle = new javax.swing.JTextField();
         cmd_markaDuzenle = new javax.swing.JButton();
         jLabel2 = new javax.swing.JLabel();
         cmd_markaSil = new javax.swing.JButton();
+        jPanel3 = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        lst_markalar = new javax.swing.JList<>();
+        jComboBox1 = new javax.swing.JComboBox<>();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jTextField2 = new javax.swing.JTextField();
+        cmd_kaydet = new javax.swing.JButton();
+        cmd_duzenle = new javax.swing.JButton();
+        cmd_sil = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Marka Ekle");
@@ -72,7 +101,7 @@ public class frm_MarkaEkle extends javax.swing.JFrame {
                         .addComponent(jLabel1)
                         .addGap(42, 42, 42)
                         .addComponent(txt_marka, javax.swing.GroupLayout.PREFERRED_SIZE, 114, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap())
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -85,13 +114,6 @@ public class frm_MarkaEkle extends javax.swing.JFrame {
                 .addComponent(cmd_markaEkle)
                 .addContainerGap())
         );
-
-        jList1.setModel(new javax.swing.AbstractListModel<String>() {
-            String[] strings = { "Item 1", "Item 2", "Item 3", "Item 4", "Item 5" };
-            public int getSize() { return strings.length; }
-            public String getElementAt(int i) { return strings[i]; }
-        });
-        jScrollPane1.setViewportView(jList1);
 
         jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Marka Düzenle", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Tahoma", 1, 12), new java.awt.Color(51, 153, 255))); // NOI18N
 
@@ -141,30 +163,109 @@ public class frm_MarkaEkle extends javax.swing.JFrame {
                 .addContainerGap())
         );
 
+        jPanel3.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Markalar", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Tahoma", 1, 12), new java.awt.Color(0, 153, 255))); // NOI18N
+
+        lst_markalar.addListSelectionListener(new javax.swing.event.ListSelectionListener() {
+            public void valueChanged(javax.swing.event.ListSelectionEvent evt) {
+                lst_markalarValueChanged(evt);
+            }
+        });
+        jScrollPane1.setViewportView(lst_markalar);
+
+        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
+        jPanel3.setLayout(jPanel3Layout);
+        jPanel3Layout.setHorizontalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel3Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 268, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(253, Short.MAX_VALUE))
+        );
+        jPanel3Layout.setVerticalGroup(
+            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
+                .addGap(0, 11, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 433, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+
+        jLabel3.setText("Marka ID : ");
+
+        jLabel4.setText("Marka Ad :");
+
+        jTextField1.setEnabled(false);
+
+        cmd_kaydet.setText("Kaydet");
+        cmd_kaydet.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                cmd_kaydetActionPerformed(evt);
+            }
+        });
+
+        cmd_duzenle.setText("Düzenle");
+
+        cmd_sil.setText("Sil");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jLabel4)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 164, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jComboBox1, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jLabel3)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 27, Short.MAX_VALUE)
+                        .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 164, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(cmd_duzenle, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 79, Short.MAX_VALUE)
+                            .addComponent(cmd_kaydet, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(cmd_sil, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
                 .addGap(18, 18, 18)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 311, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(319, Short.MAX_VALUE))
+                .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPanel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(25, 25, 25))))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
+                .addGap(155, 155, 155)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(38, 38, 38)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 492, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(24, Short.MAX_VALUE))
+                        .addGap(32, 32, 32)
+                        .addComponent(jComboBox1, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel3))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel4))
+                        .addGap(18, 18, 18)
+                        .addComponent(cmd_kaydet)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(cmd_duzenle)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(cmd_sil))
+                    .addComponent(jPanel3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(55, Short.MAX_VALUE))
         );
 
         pack();
@@ -172,6 +273,19 @@ public class frm_MarkaEkle extends javax.swing.JFrame {
 
     private void cmd_markaDuzenleActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmd_markaDuzenleActionPerformed
         // TODO add your handling code here:
+        try {
+            String tmp = lst_markalar.getSelectedValue();
+
+            String sql = "UPDATE markalar SET marka_ad='"+txt_markaDuzenle.getText()+"' WHERE marka_ad='" + tmp + "'";
+            pst = conn.prepareStatement(sql);
+            pst.executeQuery();
+        } catch (Exception e) {
+            //   JOptionPane.showMessageDialog(null, e);
+        }
+        markalariGetir();
+        txt_markaDuzenle.setText(null);
+        
+        
     }//GEN-LAST:event_cmd_markaDuzenleActionPerformed
 
     private void cmd_markaSilActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmd_markaSilActionPerformed
@@ -179,28 +293,38 @@ public class frm_MarkaEkle extends javax.swing.JFrame {
     }//GEN-LAST:event_cmd_markaSilActionPerformed
 
     private void cmd_markaEkleActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmd_markaEkleActionPerformed
-         String sql="INSERT INTO markalar (marka_id,marka_ad) VALUES ('', "+ txt_marka.getText() +"')";
-    
-     
-
-
-
         try {
-             stmt = conn.createStatement();
-             pst.executeUpdate(sql);
 
-
-           // pst.executeUpdate(sql);
-            
-            //pst =conn.prepareStatement(sql);
-            //rs=pst.executeUpdate();
-            
-          // pst.executeQuery(sql);
-           
+            String sql = "INSERT INTO markalar (marka_ad) VALUES ('" + txt_marka.getText() + "')";
+            pst = conn.prepareStatement(sql);
+            pst.executeQuery();
         } catch (Exception e) {
-                JOptionPane.showMessageDialog(null, e);
+            // JOptionPane.showMessageDialog(null, e);
         }
+        markalariGetir();
     }//GEN-LAST:event_cmd_markaEkleActionPerformed
+
+    private void lst_markalarValueChanged(javax.swing.event.ListSelectionEvent evt) {//GEN-FIRST:event_lst_markalarValueChanged
+        // TODO add your handling code here:
+        //JOptionPane.showMessageDialog(null, "kenan");
+        String tmp = lst_markalar.getSelectedValue();
+        //JOptionPane.showMessageDialog(null,tmp);
+        String sql = "select * from markalar where marka_ad=?";
+        try {
+            pst = conn.prepareCall(sql);
+            pst.setString(1, tmp);
+            rs = pst.executeQuery();
+            if (rs.next()) {
+                String add = rs.getString("marka_ad");
+                txt_markaDuzenle.setText(add);
+            }
+        } catch (Exception e) {
+        }
+    }//GEN-LAST:event_lst_markalarValueChanged
+
+    private void cmd_kaydetActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmd_kaydetActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_cmd_kaydetActionPerformed
 
     /**
      * @param args the command line arguments
@@ -238,15 +362,24 @@ public class frm_MarkaEkle extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton cmd_duzenle;
+    private javax.swing.JButton cmd_kaydet;
     private javax.swing.JButton cmd_markaDuzenle;
     private javax.swing.JButton cmd_markaEkle;
     private javax.swing.JButton cmd_markaSil;
+    private javax.swing.JButton cmd_sil;
+    private javax.swing.JComboBox<String> jComboBox1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
-    private javax.swing.JList<String> jList1;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JList<String> lst_markalar;
     private javax.swing.JTextField txt_marka;
     private javax.swing.JTextField txt_markaDuzenle;
     // End of variables declaration//GEN-END:variables
